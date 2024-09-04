e203b544e61e29e9dd33bca14c593a7b2d5065ea
Ghifary13 <2210631170016@student.unsika.ac.id>
2024-09-04 15:37:43 +0700

    tugas mingguan

---
+++ pertemuan_3/makmur-jaya/routes/web.php
@@ -0,0 +1,63 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Symfony\Component\HttpKernel\Profiler\Profile;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hello', function () {
+    return 'Hello, World';
+});
+
+Route::get('/user/{id}', function ($id) {
+    return "User ID: ". $id;
+});
+
+Route::get('/user/{name?}', function ($name ='Guest') {
+    return "Hello," .$name;
+});
+
+Route::get('/profile', function () {
+    return 'This Is The Profile Page.';
+})->name('profile');
+
+Route::get('/redirect-to-profile', function () {
+    return redirect()->router('profile');
+});
+
+Route :: prefix('admin')->group(function () {
+    Route :: get('/dashboard', function () {
+    return 'Admin Dashboard';
+    });
+
+    Route :: get('/profile', function () {
+        return 'Admin Profile';
+    });
+});
+
+
+Route::get('/dashboard', function () {
+    return 'Welcome to your dashboard!';
+})->middleware('auth');
+
+Route::resource('posts','PostController');
+
+
+
+Route::get('/penjumlahan/{angka1}/{angka2}', function ($angka1, $angka2) {
+    $hasil = $angka1 + $angka2;
+    return 'Hasil Penjumlahan: ' . $hasil;
+});
